97ce607ef00ec8ba608048a05090ec898acc3531
Alex Cipollone <alex.cipollone10@gmail.com>
2023-04-25 22:47:56 -0400

    insert

--- learning-algorithms/src/algo/insertionsort/App.java
+++ learning-algorithms/src/algo/insertionsort/App.java
@@ -1,11 +1,25 @@
 package algo.insertionsort;
 
 public class App {
+    
     public static void main(String[] args) {
-        System.out.println(insertionSort(new int[] {4,79,2,69,25,26,34,42,9}));
+       int myArray[] = insertionSort(new int[] {4,79,2,69,25,26,34,42,9});
+
+       for (int i = 0; i < myArray.length; i++){
+        System.out.println(myArray[i]);
+       }
     }
 
     public static int[] insertionSort(int [] a){
+        for (int i = 1; i < a.length; i++){
+            int element = a[i]; // element variable contains the data we intend on bringing over to the sorted area
+            int j = i - 1; // j variable points to the index position of the last value in the sorted area
+            while(j >= 0 && a[j] > element){ 
+                a[j + 1] = a[j]; 
+                j--;
+            }
+           a[j+1] = element;
+        }
         return a;
     }
 }
